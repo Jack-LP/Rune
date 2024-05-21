73e9aba74ebc3e044ac2f692a6ed87a05d25f231
Jack <jackpaget1@gmail.com>
2024-05-21 16:43:34 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='bg-neutral-800 h-screen text-white flex items-center justify-center'></div>
+  );
+};
+
+export default App;
